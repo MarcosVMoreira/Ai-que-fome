950620ae4302fe7a73c0dbd832c5edf9f37d202e
OtavioPalma <otaviopalma@gmail.com>
2020-08-13 21:26:15 -0300

    Finished: Login

--- frontend/src/pages/login/login.page.js
+++ frontend/src/pages/login/login.page.js
@@ -1,69 +1,67 @@
 import React from 'react';
 
-import Grid from '@material-ui/core/Grid';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import { Typography, Button } from '@material-ui/core';
-import FacebookIcon from '@material-ui/icons/Facebook';
-import MailIcon from '@material-ui/icons/Mail';
+import {
+  Card,
+  Grid,
+  CardActions,
+  CardContent,
+  Button,
+  TextField,
+} from '@material-ui/core';
 
-import logo from '../../assets/icons/logo.png';
+import logo from '../../assets/icons/logo.webp';
 
 import './login.page.scss';
 
 const LoginPage = () => {
   return (
     <div className="container">
-      <Grid container justify="center">
-        <Grid item sm={12} className="container_header">
-          <img src={logo} className="container_header--logo" />
-        </Grid>
+      <Grid container className="container_body">
+        <Grid item xs={12} sm={5}>
+          <Card className="container_body--card">
+            <CardContent className="container_body--card_content">
+              <Grid item className="container_body--card_header">
+                <img
+                  src={logo}
+                  alt="logotipo ifood"
+                  className="container_body--card_header--logo"
+                />
+              </Grid>
 
-        <Grid container justify="space-around" alignItems="center" spacing={3}>
-          <Grid item>Imagem!a</Grid>
-
-          <Grid item>
-            <Card className="container_body--card">
-              <CardContent className="container_body--card_content">
-                <Typography variant="h4" color="textPrimary" align="center">
+              <Grid container item justify="center">
+                <span className="container_body--card_content--title">
                   Falta pouco para matar sua fome!
-                </Typography>
+                </span>
 
-                <Typography variant="h5" color="textSecondary" align="center">
-                  Como deseja continuar?
-                </Typography>
-              </CardContent>
+                <span className="container_body--card_content--subtitle">
+                  Digite seu email para continuar
+                </span>
 
-              <CardActions className="container_body--card_actions">
-                <Grid container justify="center">
-                  <Grid xs={12} sm={8}>
-                    <Button
-                      size="large"
-                      color="primary"
-                      variant="contained"
-                      fullWidth
-                      startIcon={<FacebookIcon />}
-                      className="container_body--card_actions--button"
-                    >
-                      Continuar com Facebook
-                    </Button>
-                  </Grid>
-                  <Grid xs={12} sm={8}>
-                    <Button
-                      size="large"
-                      variant="contained"
-                      fullWidth
-                      startIcon={<MailIcon />}
-                      className="container_body--card_actions--button"
-                    >
-                      Email
-                    </Button>
-                  </Grid>
+                <Grid container item justify="center" xs={12} sm={9}>
+                  <TextField
+                    label="Email"
+                    variant="outlined"
+                    className="container_body--card_content--input"
+                  />
                 </Grid>
-              </CardActions>
-            </Card>
-          </Grid>
+              </Grid>
+            </CardContent>
+
+            <CardActions className="container_body--card_actions">
+              <Grid container item justify="center">
+                <Grid container item justify="center" xs={12} sm={9}>
+                  <Button
+                    size="large"
+                    color="primary"
+                    variant="contained"
+                    className="container_body--card_actions--button"
+                  >
+                    Entrar
+                  </Button>
+                </Grid>
+              </Grid>
+            </CardActions>
+          </Card>
         </Grid>
       </Grid>
     </div>
