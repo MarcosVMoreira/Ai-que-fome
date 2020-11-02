036f4dcf68262039263dcdc9ac4d22add5b14954
Marcos Vinícius Moreira <contato.marcosvmoreira@gmail.com>
2020-11-02 18:55:52 -0300

    chore(fix bean validator) (#11)
    * fix: fixing bean validator for DTO

--- backend/customer-service/src/main/java/com/ifood/customer/endpoint/model/dto/AddressDTO.java
+++ backend/customer-service/src/main/java/com/ifood/customer/endpoint/model/dto/AddressDTO.java
@@ -2,6 +2,9 @@ package com.ifood.customer.endpoint.model.dto;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.List;
 
 @Data
@@ -9,18 +12,27 @@ public class AddressDTO {
 
     private String id;
 
+    @NotEmpty(message = "422.002")
     private String streetName;
 
+    @NotNull(message = "422.002")
     private Long streetNumber;
 
+    @NotEmpty(message = "422.002")
     private String district;
 
+    @NotEmpty(message = "422.002")
     private String city;
 
+    @NotEmpty(message = "422.002")
     private String neighborhood;
 
+    @NotEmpty(message = "422.002")
     private String country;
 
+    @NotEmpty(message = "422.002")
+    @Size(max = 8, message = "422.004")
+    @Size(min = 8, message = "422.004")
     private String postalCode;
 
     private List<String> coordinates;
@@ -30,5 +42,4 @@ public class AddressDTO {
     private String complement;
 
     private String refPoint;
-
 }
