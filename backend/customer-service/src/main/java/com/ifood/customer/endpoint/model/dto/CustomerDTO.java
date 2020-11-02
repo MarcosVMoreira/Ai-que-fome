b93d6f3826cd02c134197a65ae255c41c99d2405
Marcos Moreira <contato.marcosvmoreira@gmail.com>
2020-11-02 18:40:08 -0300

    fix: fixing bean validator for DTO

--- backend/customer-service/src/main/java/com/ifood/customer/endpoint/model/dto/CustomerDTO.java
+++ backend/customer-service/src/main/java/com/ifood/customer/endpoint/model/dto/CustomerDTO.java
@@ -3,6 +3,8 @@ package com.ifood.customer.endpoint.model.dto;
 import com.ifood.customer.endpoint.model.entity.Address;
 import lombok.Data;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
 import java.util.List;
 
 @Data
@@ -10,14 +12,20 @@ public class CustomerDTO {
 
     private String id;
 
+    @NotEmpty(message = "422.002")
     private String name;
 
+    @NotEmpty(message = "422.002")
     private String phone;
 
+    @NotEmpty(message = "422.002")
+    @Email(message = "422.003")
     private String email;
 
+    @NotEmpty(message = "422.002")
     private String taxPayerIdentificationNumber;
 
+    @NotEmpty(message = "422.002")
     private List<Address> addresses;
 
 }
