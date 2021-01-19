0f4084168d98a780b7eb18b3bc94fdc7fbf0ab56
Marcos Vinicius Moreira <marcos.moreira@sensedia.local>
2021-01-19 20:09:33 -0300

    Fix SKU não adicionando ID de forma automática

--- backend/merchant-service/src/main/java/com/ifood/customer/endpoint/model/entity/SKU.java
+++ backend/merchant-service/src/main/java/com/ifood/customer/endpoint/model/entity/SKU.java
@@ -3,6 +3,7 @@ package com.ifood.customer.endpoint.model.entity;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -33,4 +34,7 @@ public class SKU {
 
     private List<Option> options;
 
+    public SKU () {
+        id = new ObjectId().toString();
+    }
 }
